be55f488a651ec076ede27d457ff10e7b4d9f2ed
Mark Grebe <grebe@ucmo.edu>
2022-11-05 15:47:08 -0500

    Added HW6 Starter Code

---
+++ HW06/main.c
@@ -0,0 +1,29 @@
+#include <rtthread.h>
+
+/* Global Variables for Semaphores */
+rt_sem_t sem1;
+rt_sem_t sem2;
+/* Global Variables for Threads */
+rt_thread_t tid1;
+rt_thread_t tid2;
+
+void thread1(void *parameter)
+    {
+
+    }
+
+void thread2(void *parameter)
+    {
+
+    }
+
+int main(void)
+{
+    rt_kprintf("Hello, RT-Thread!\n");
+
+    /* Create the Semphores */
+
+    /* Create the Threads */
+
+}
+
